a52f0a42ef543c7b0d7a41b02de8d64f916dad0c
mohamedkhaled255 <mohamedkha243e@gmail.com>
2025-10-27 21:22:48 +0300

    show modal bottom sheet

--- lib/widgets/notes_view_body.dart
+++ lib/widgets/notes_view_body.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/widgets/custom_app_bar.dart';
 
+import 'package:notes_app/widgets/notes_list_view.dart';
+
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 10),
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
       child: Column(
-        children:const [
-          SizedBox(height: 15,),
+        children: const [
+          SizedBox(height: 15),
           CustomAppBar(),
+          SizedBox(height: 20),
+
+          Expanded(child: NotesListView())
 
         ],
       ),
